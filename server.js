91ea1623f4708191562cd19c511127bc78a0e9b2
Eren Aygün <117078263+erenaygunn@users.noreply.github.com>
2024-01-01 02:49:38 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const corsOptions = {
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", 'https://quotterr.netlify.app/');
+  res.setHeader("Access-Control-Allow-Origin", 'https://quotterr.netlify.app');
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();

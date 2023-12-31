8d7e267ebe6c4c529182fa9a36c2b4ddb69f5fdf
Eren Aygün <117078263+erenaygunn@users.noreply.github.com>
2024-01-01 02:05:47 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const corsOptions = {
     } else {
       callback(new Error("Not allowed by CORS"))
     }
-  }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
 };
 
 app.use(express.json());

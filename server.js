bd6bd89c3a02658fc0954ab37248ffd19e6ed170
Eren Aygün <117078263+erenaygunn@users.noreply.github.com>
2024-01-01 12:14:56 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", 'https://quotterr.netlify.app');
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization")
   next();
 })
 
